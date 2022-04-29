040a83e208035ad70402f50d76aba345fde48179
Chenxing Luo <chenxing.luo@gmail.com>
2022-04-29 11:23:37 -0400

    Add TOC generation code

---
+++ scripts/toc4docs.py
@@ -0,0 +1,39 @@
+import re
+from pathlib import Path
+from bs4 import BeautifulSoup
+from lxml.html import fromstring
+from tqdm.cli import tqdm
+
+DOCSET_ROOT = 'qe.docset'
+DOCSET_DOCS = Path(f'{DOCSET_ROOT}/Contents/Resources/Documents')
+
+def insert_anchor(soup: BeautifulSoup, name: str, type: str, id: str):
+    id = id.lstrip("#")
+    tag = soup.find(None, {"name": id.lstrip("#")})
+    anchor = BeautifulSoup(f"<a name='//apple_ref/cpp/{type}/{name}' class='dashAnchor' />")
+    print(tag, anchor)
+    tag.insert_after(anchor)
+
+if __name__ == "__main__":
+
+    # Input descriptions
+
+    for file in tqdm(DOCSET_DOCS.glob("Doc/INPUT_*.html")):
+
+        path = file.relative_to(DOCSET_DOCS)
+        soup = BeautifulSoup(open(file), features="lxml")
+
+        for a in soup.select("td > blockquote blockquote > p > a"):
+            try:
+                insert_anchor(soup, a.text.lstrip("&").rstrip(":"), "Section", a["href"])
+            except KeyError:
+                pass
+
+        for a in soup.select("td > blockquote > blockquote blockquote > a"):
+            try:
+                insert_anchor(soup, a.text, "Parameter", a["href"])
+            except KeyError:
+                pass
+    
+        with open(file, "w") as fp:
+            fp.write(str(soup))
